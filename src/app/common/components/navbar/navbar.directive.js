99f29babc2556ce377cf7eb0698b6eac980390db
dibosh <dibosh@newscred.com>
2017-11-27 00:06:36 +0600

    Add replace: true for navbar directive

--- src/app/common/components/navbar/navbar.directive.js
+++ src/app/common/components/navbar/navbar.directive.js
@@ -3,6 +3,7 @@ export function NavbarDirective() {
 
   let directive = {
     restrict: 'E',
+    replace: true,
     templateUrl: 'app/common/components/navbar/navbar.html',
     scope: {
       brandUrl: '@',
