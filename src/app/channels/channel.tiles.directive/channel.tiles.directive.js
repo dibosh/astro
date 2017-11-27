a34507616bb609765cea9f6b9451a6f9666b87cc
dibosh <dibosh@newscred.com>
2017-11-28 01:10:32 +0600

    Add pagination for channels

--- src/app/channels/channel.tiles.directive/channel.tiles.directive.js
+++ src/app/channels/channel.tiles.directive/channel.tiles.directive.js
@@ -7,12 +7,62 @@ export function ChannelTilesDirective() {
     templateUrl: 'app/channels/channel.tiles.directive/channel.tiles.html',
     scope: {
       isLoading: '=',
-      channels: '='
+      channels: '=',
+      totalChannels: '=',
+      pageSize: '=',
+      paginate: '&'
     },
     link: function (scope) {
+      scope.paginationNavs = [
+        {
+          direction: 'left',
+          icon: 'glyphicon-chevron-left'
+        },
+        {
+          direction: 'right',
+          icon: 'glyphicon-chevron-right'
+        }
+      ];
+
+      scope.paginationConfig = {
+        pageSize: parseInt(scope.pageSize),
+        pageStart: 1,
+        currentPage: 1
+      };
+
+      scope.getCurrentlyShowingInfo = function () {
+        var currPos = (scope.paginationConfig.currentPage - 1) * scope.paginationConfig.pageSize + 1;
+        var endPos = currPos + scope.paginationConfig.pageSize - 1;
+        currPos = currPos <= 0 ? scope.paginationConfig.pageStart : currPos;
+        endPos = endPos > scope.totalChannels ? scope.totalChannels : endPos;
+        return `Showing <strong>${currPos}-${endPos}</strong> of <strong>${scope.totalChannels}</strong> channels`;
+      };
+
+      scope.onPaginate = function (direction) {
+        var currentPage = scope.paginationConfig.currentPage;
+
+        direction === 'left' ? currentPage -= 1 :  currentPage+= 1;
+        
+        if (currentPage < scope.paginationConfig.pageStart) {
+          currentPage = scope.paginationConfig.pageStart;
+        }
+
+        if (currentPage > scope.paginationConfig.pageEnd) {
+          currentPage = scope.paginationConfig.pageEnd;
+        }
+
+        scope.paginationConfig.currentPage = currentPage;
+
+        scope.paginate({pageSize: scope.paginationConfig.pageSize, pageNumber: scope.paginationConfig.currentPage});
+      };
+
       scope.toggleFavoriteFor = function (channel) {
         channel.isFavorite = !channel.isFavorite;
       };
+
+      scope.$watch('totalChannels', function (totalChannels) {
+        scope.paginationConfig.pageEnd = Math.ceil(totalChannels / scope.paginationConfig.pageSize);
+      });
     }
   };
 
--- src/app/channels/channels.controller.js
+++ src/app/channels/channels.controller.js
@@ -1,12 +1,20 @@
 class ChannelsController {
 
   constructor(channelsService) {
-    let self = this;
-    self.isLoading = true;
+    this.pageSize = 8;
+    this.channelsService = channelsService;
+    this.paginate(this.pageSize, 1);
+  }
 
-    channelsService.getChannels(8, 1)
+  paginate(pageSize, pageNumber) {
+    var self = this;
+    self.isLoading = true;
+    self.channelsService.getChannels(pageSize, pageNumber)
       .then((response) => {
         self.channels = response.channels;
+        if (!self.totalChannels) {
+          self.totalChannels = response.numFound;
+        }
       })
       .finally(() => {
         self.isLoading = false;
