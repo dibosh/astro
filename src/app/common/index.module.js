af581d64c08f6151b350d8595e846646ba82f31d
dibosh <dibosh@newscred.com>
2017-11-29 00:32:22 +0600

    Include new directives

--- src/app/common/index.module.js
+++ src/app/common/index.module.js
@@ -1,6 +1,10 @@
 import {NavbarDirective} from './components/navbar/navbar.directive';
+import {DropdownDirective} from './components/dropdown/dropdown.directive'
+import {OffClickDirective} from './components/off.click.directive'
 import SimpleHTTPService from './simple.http.service';
 
 angular.module('astro.common', [])
   .directive('acmeNavbar', NavbarDirective)
+  .directive('dropdown', DropdownDirective)
+  .directive('offClick', OffClickDirective)
   .service('simpleHttpService', SimpleHTTPService);
