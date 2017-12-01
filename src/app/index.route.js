7ab088483589b472939c3359918afd1b20bdee9f
dibosh <dibosh@newscred.com>
2017-12-02 01:37:21 +0600

    Resolve all the channels at the beginning

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,13 +1,27 @@
 export function routerConfig ($stateProvider, $urlRouterProvider) {
   'ngInject';
   $stateProvider
+    .state('root', {
+      abstract: true,
+      template: '<div ui-view></div>',
+      resolve: {
+        channels: (_, $q, ChannelBasket) => {
+          let preloaded = !_.isEmpty(ChannelBasket.channels);
+          return  preloaded ? $q.when(ChannelBasket.channels) : ChannelBasket.retrieveChannels();
+        }
+      }
+    });
+
+  $stateProvider
     .state('channels', {
+      parent: 'root',
       url: '/channels',
       templateUrl: 'app/channels/channels.html',
       controller: 'ChannelsController',
       controllerAs: 'vm'
     })
     .state('tv-guide', {
+      parent: 'root',
       url: '/tv-guide',
       templateUrl: 'app/tv.guide/tv.guide.html',
       controller: 'TVGuideController',
