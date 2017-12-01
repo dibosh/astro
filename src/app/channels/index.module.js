fabb5a4a511869d067a09927b50bb5447595d6e7
dibosh <dibosh@newscred.com>
2017-12-01 23:59:27 +0600

    Move channels.service to common

--- src/app/channels/index.module.js
+++ src/app/channels/index.module.js
@@ -1,10 +1,8 @@
 import ChannelsController from './channels.controller';
 import {ChannelTilesDirective} from './channel.tiles.directive/channel.tiles.directive';
 import ChannelDetailsModalInstanceController from './channel.details.modal.instance.controller';
-import ChannelsService from './channels.service';
 
 angular.module('astro.channels', [])
   .controller('ChannelsController', ChannelsController)
   .controller('ChannelDetailsModalInstanceController', ChannelDetailsModalInstanceController)
-  .service('channelsService', ChannelsService)
   .directive('channelTiles', ChannelTilesDirective);
--- src/app/common/index.module.js
+++ src/app/common/index.module.js
@@ -1,10 +1,12 @@
 import {NavbarDirective} from './components/navbar/navbar.directive';
 import {DropdownDirective} from './components/dropdown/dropdown.directive'
 import {OffClickDirective} from './components/off.click.directive'
+import ChannelsService from './services/channels.service';
 import SimpleHTTPService from './services/simple.http.service';
 
 angular.module('astro.common', [])
   .directive('acmeNavbar', NavbarDirective)
   .directive('dropdown', DropdownDirective)
   .directive('offClick', OffClickDirective)
-  .service('simpleHttpService', SimpleHTTPService);
+  .service('simpleHttpService', SimpleHTTPService)
+  .service('channelsService', ChannelsService);
