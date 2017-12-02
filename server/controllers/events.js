672739cb7e4c825d64b39caec2c593b7efb9b477
dibosh <dibosh@newscred.com>
2017-12-03 01:18:25 +0600

    Add events endpoint for categorized search

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -7,9 +7,64 @@ router.get('/', function (req, res) {
   utils.handleHttpRequestPromise(_makeEventListResponse(req.query), res);
 });
 
+router.get('/forChannelCategory', function (req, res) {
+  utils.handleHttpRequestPromise(_makeEventListResponseBasedOnChannelCategory(req.query), res);
+});
+
 function _makeEventListResponse(query) {
-  return utils.makeHttpRequest(null, eventsUrl,
-    utils.prepareEventsRequestConfig(query.channelIds, query.startDate, query.endDate, utils.configVersion))
+  if (!query || !query.channelIds || !query.startDate || !query.endDate) {
+    return new Promise(function (resolve) {
+      resolve({
+        status: 400,
+        body: {
+          message: 'channelIds, startDate, endDate are mandatory query params.'
+        }
+      });
+    });
+  }
+  var requestConfig = {
+    channelId: query.channelIds,
+    periodStart: query.startDate,
+    periodEnd: query.endDate
+  };
+
+  return utils.makeHttpRequest(null, eventsUrl, requestConfig)
+    .then(function (response) {
+      var rawEvents = response.body.getevent;
+      var eventsResp = [];
+      rawEvents.forEach(function (event) {
+        eventsResp.push(utils.createEventResponse(event));
+      });
+
+      return {
+        status: response.body.responseCode,
+        body: {
+          numFound: rawEvents.length,
+          events: eventsResp
+        }
+      };
+    });
+}
+
+function _makeEventListResponseBasedOnChannelCategory(query) {
+  if (!query || !query.channelCategory || !query.startDate || !query.endDate) {
+    return new Promise(function (resolve) {
+      resolve({
+        status: 400,
+        body: {
+          message: 'channelCategory, startDate, endDate are mandatory query params.'
+        }
+      });
+    });
+  }
+  var requestConfig = {
+    channelCategory: query.channelCategory,
+    periodStart: query.startDate,
+    periodEnd: query.endDate,
+    live: false,
+    highlight: false
+  };
+  return utils.makeHttpRequest(null, eventsUrl, requestConfig)
     .then(function (response) {
       var rawEvents = response.body.getevent;
       var eventsResp = [];
