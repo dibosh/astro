c53834c1789a6b99f7910272978711e75b5eb797
dibosh <dibosh@newscred.com>
2017-12-02 23:38:18 +0600

    Make channelId unique instead of stbnumber

--- server/models/channel.js
+++ server/models/channel.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var channelSchema = new Schema({
-  channelId: {type: Number, required: true},
+  channelId: {type: Number, required: true, unique: true},
   title: {type: String, required: true},
   description: {type: String},
   category: {type: String},
-  setTopBoxNumber: {type: Number, required: true, unique: true},
+  setTopBoxNumber: {type: Number, required: true},
   language: {type: String},
   color: {type: String},
   logo: {type: String},
