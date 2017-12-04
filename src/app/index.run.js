9cd89beabb32d89eb031f1f86db4f566f0d34d91
dibosh <dibosh@newscred.com>
2017-12-05 00:00:03 +0600

    Setup fb sdk with app id in angular app

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,4 +1,35 @@
-export function runBlock ($log) {
+/* global FB:false */
+
+export function runBlock($log, $window, $document) {
   'ngInject';
   $log.debug('runBlock end');
+
+  $window.fbAsyncInit = function () {
+    FB.init({
+      appId: '114659072652866',
+      status: false,
+      cookie: true,
+      xfbml: false,
+      version: 'v2.8'
+    });
+  };
+
+  function appendFBSDK(d) {
+    var js,
+      id = 'facebook-jssdk',
+      ref = d.getElementsByTagName('script')[0];
+
+    if (d.getElementById(id)) {
+      return;
+    }
+
+    js = d.createElement('script');
+    js.id = id;
+    js.async = true;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js';
+
+    ref.parentNode.insertBefore(js, ref);
+  }
+
+  appendFBSDK($document[0]);
 }
