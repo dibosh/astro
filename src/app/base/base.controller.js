994ac6b841331e3128a940d4ec4732052621c40d
dibosh <dibosh@newscred.com>
2017-12-07 01:59:39 +0600

    Add all the support to get the authenticated user

--- src/app/base/base.controller.js
+++ src/app/base/base.controller.js
@@ -1,5 +1,5 @@
 class BaseController {
-  constructor ($rootScope) {
+  constructor ($rootScope, $auth, UserBasket) {
     this.isStateLoading = false;
     this.navs = [
       {
@@ -12,6 +12,20 @@ class BaseController {
       }
     ];
 
+    if (UserBasket.shouldFetch()) {
+      UserBasket.fetch()
+        .then((user) => {
+          this.user = user;
+        })
+        .catch((err) => {
+          if (err.status === 404) {
+            $auth.logout();
+          }
+        });
+    } else {
+      this.user = UserBasket.user;
+    }
+
     let stateChangeStartListener = $rootScope.$on('$stateChangeStart', () => {
       this.isStateLoading = true;
     });
@@ -20,19 +34,27 @@ class BaseController {
       this.isStateLoading = false;
     });
 
-    $rootScope.$on('$destroy', function () {
+    $rootScope.$on('$destroy', () => {
       stateChangeEndListener();
       stateChangeStartListener();
     });
 
+    $rootScope.$on(UserBasket.USER_UPDATED_NOTIFY, (data) => {
+      this.user = data.user;
+    });
 
     this.brand = {
       url: 'https://www.astro.com.my/',
       logo: 'app/assets/images/logo.png'
-    }
+    };
+
+    this.logOut = () => {
+      $auth.logout();
+      this.user = null;
+    };
   }
 }
 
-BaseController.$inject = ['$rootScope'];
+BaseController.$inject = ['$rootScope', '$auth', 'UserBasket'];
 
 export default BaseController;
