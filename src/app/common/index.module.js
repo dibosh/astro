ef505a9f07c90f480e6170751b59ac220f9dc85d
dibosh <dibosh@newscred.com>
2017-12-07 01:14:09 +0600

    Define user service

--- src/app/common/index.module.js
+++ src/app/common/index.module.js
@@ -3,9 +3,11 @@ import {DropdownDirective} from './components/dropdown/dropdown.directive';
 import {OffClickDirective} from './components/off.click.directive';
 import {PaginationDirective} from './components/pagination/pagination.directive';
 import ChannelsService from './services/channels.service';
+import UserService from './services/user.service';
 import SimpleHTTPService from './services/simple.http.service';
 import ChannelBasket from './services/channel.basket';
 import EventsBasket from './services/events.basket';
+import UserBasket from './services/user.basket';
 
 angular.module('astro.common', [])
   .directive('acmeNavbar', NavbarDirective)
@@ -14,5 +16,7 @@ angular.module('astro.common', [])
   .directive('pagination', PaginationDirective)
   .service('simpleHttpService', SimpleHTTPService)
   .service('channelsService', ChannelsService)
+  .service('userService', UserService)
   .service('ChannelBasket', ChannelBasket)
-  .service('EventsBasket', EventsBasket);
+  .service('EventsBasket', EventsBasket)
+  .service('UserBasket', UserBasket);
--- src/app/common/services/user.service.js
+++ src/app/common/services/user.service.js
@@ -1,36 +1,69 @@
 class UserService {
-  constructor(simpleHttpService, $auth, UserBasket) {
+  constructor(simpleHttpService, $auth, UserBasket, $q, _) {
     this.httpService = simpleHttpService;
     this.$auth = $auth;
+    this.UserBasket = UserBasket;
+    this.$q = $q;
+    this._ = _;
   }
 
   getCurrentUser() {
-    let headers = {
-      Authorization: this.$auth.getToken()
-    };
-    
-    return this.httpService.makeGETRequest('user/me', null, headers)
+    return this.httpService.makeGETRequest('user/me', null, this._getAuthHeader())
       .then((response) => {
         return response.user;
       });
   }
-  
+
   addChannelToFavorites(channelId) {
-    
+    let deferred = this.$q.defer();
+    let currentUser = this._checkCurrentUser(deferred);
+
+    let favChannelIds = currentUser.favoriteChannelIds || [];
+    favChannelIds.push(channelId);
+
+    this._updateFavoriteChannels(favChannelIds, deferred);
+
+    return deferred.promise;
   }
 
-  makePUTRequest(endpoint, body) {
-    let config = {
-      method: 'PUT',
-      url: this.baseUrl + endpoint,
-      data: body
+  removeChannelFromFavorites(channelId) {
+    let deferred = this.$q.defer();
+    let currentUser = this._checkCurrentUser(deferred);
+    let favChannelIds = currentUser.favoriteChannelIds || [];
+    let removableChannelIndex = _.indexOf(favChannelIds, channelId);
+    favChannelIds.splice(removableChannelIndex, 1);
+
+    this._updateFavoriteChannels(favChannelIds, deferred);
+
+    return deferred.promise;
+  }
+
+  _updateFavoriteChannels(channelIds, deferred) {
+    this.httpService.makePUTRequest('user/favoriteChannels', {channelIds: channelIds}, this._getAuthHeader())
+      .then((response) => {
+        return deferred.resolve(response);
+      })
+      .catch((err) => {
+        return deferred.reject(err);
+      });
+  }
+
+  _checkCurrentUser(deferred) {
+    let currentUser = this.UserBasket.user;
+    if (this._.isUndefined(currentUser)) {
+      return deferred.reject({message: 'No current user.'});
+    }
+
+    return currentUser;
+  }
+
+  _getAuthHeader() {
+    return {
+      Authorization: this.$auth.getToken()
     };
-    return this.$http(config).then((response)=> {
-      return response.data;
-    });
   }
 }
 
-UserService.$inject = ['simpleHttpService', '$auth', 'UserBasket'];
+UserService.$inject = ['simpleHttpService', '$auth', 'UserBasket', '$q', '_'];
 
 export default UserService;
